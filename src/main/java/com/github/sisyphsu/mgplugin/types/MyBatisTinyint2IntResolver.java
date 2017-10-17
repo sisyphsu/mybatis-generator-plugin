6b402f59f5e29ded11feb147761455d4b2a33a80
linlin.su <linlin.su@qunar.com>
2017-10-17 23:27:28 +0800

    rename class's name

--- src/main/java/com/github/sisyphsu/mgplugin/types/MyBatisTinyint2IntResolver.java
+++ src/main/java/com/github/sisyphsu/mgplugin/types/MyBatisTinyint2IntResolver.java
@@ -7,11 +7,11 @@ import org.mybatis.generator.internal.types.JavaTypeResolverDefaultImpl;
 import java.sql.Types;
 
 /**
- * Support type conversion: TINYINT(db) <-> Integer(java)
+ * Support type conversion: TINYINT(db) == Integer(java)
  * <p>
  * Created by sulin on 2017-10-17 23:08:14.
  */
-public class MyBatisTINYINT2IntResolver extends JavaTypeResolverDefaultImpl {
+public class MyBatisTinyint2IntResolver extends JavaTypeResolverDefaultImpl {
     
     @Override
     public FullyQualifiedJavaType calculateJavaType(IntrospectedColumn introspectedColumn) {
